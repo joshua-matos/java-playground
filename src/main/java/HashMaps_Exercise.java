aecf695b1b912e3b72214d5fb6383bb5b8fd033f
Josh <josh@joshuamatos.com>
2022-03-03 11:10:16 -0600

    Hash maps added

--- src/main/java/HashMaps_Exercise.java
+++ src/main/java/HashMaps_Exercise.java
@@ -1,3 +1,19 @@
+import java.util.HashMap;
+
 public class HashMaps_Exercise {
+
+	public static void main(String... args){
+
+
+	}
+
+	public HashMap<Integer, String> genHashMap() {
+		HashMap<Integer, String> generateHashMap
+				= new HashMap<>();
+		generateHashMap.put(747, "Airplane");
+		generateHashMap.put(10, "Laphroaig");
+
+		return generateHashMap;
+	}
 
 }
