3fa48ae66d366c4576d0f5826a7e6fbb74cddd3d
Josh <josh@joshuamatos.com>
2022-03-03 10:58:53 -0600

    initial commit

---
+++ src/main/java/ArrayList_exercise.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ArrayList_exercise {
+
+
+
+
+	public static void main(String... args){
+		String[] myStringArray = new String[]{"B", "C",  "E", "F", "D","A"};
+
+		ArrayList_exercise sortedStrings = new ArrayList_exercise();
+
+		System.out.println(sortedStrings.sortArray(myStringArray));
+
+
+	}
+	// Implement the sortArray method below
+	public ArrayList<String> sortArray(String[] inputArray) {
+		ArrayList<String> sortMyArray = new ArrayList<>(Arrays.asList(inputArray));
+		sortMyArray.sort(null);
+		return sortMyArray;
+	}
+}
---
+++ src/main/java/HashMaps_Exercise.java
@@ -0,0 +1,3 @@
+public class HashMaps_Exercise {
+
+}
