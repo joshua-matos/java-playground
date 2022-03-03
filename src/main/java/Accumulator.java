201dd5abfeb3596735a4c798f0fdfafde8c4c937
Josh <josh@joshuamatos.com>
2022-03-03 14:41:53 -0600

    Renamed class

--- src/main/java/Accumulator.java
+++ src/main/java/Accumulator.java
@@ -1,32 +1,27 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.StringJoiner;
-
 public class Accumulator {
 
-	 public static void main(String... args) {
-		 Accumulator testAccumelator = new Accumulator();
-		 String[] myArray = {};
-		 System.out.flush();
-		 System.out.println(testAccumelator.toSentence(myArray) + "\n");
-	 }
+	public static void main(String... args) {
+		Accumulator testAccumulator = new Accumulator();
+		String[] myArray = {};
+		System.out.flush();
+		System.out.println(testAccumulator.toSentence(myArray) + "\n");
+	}
 
-	 public String toSentence(String[] arrayOfStrings){
-		 String joinString = "";
-		 if(arrayOfStrings == null || arrayOfStrings.length == 0) return "";
-		 if(arrayOfStrings.length == 1){
-			 joinString = arrayOfStrings[0];
+	public String toSentence(String[] arrayOfStrings) {
+		String joinString = "";
+		if (arrayOfStrings == null || arrayOfStrings.length == 0) return "";
+		if (arrayOfStrings.length == 1) {
+			joinString = arrayOfStrings[0];
 
-		 } else if(arrayOfStrings.length == 2){
-				joinString = arrayOfStrings[0] +" and "+ arrayOfStrings[1];
-		 } else {
-			 String[] temp = new String[arrayOfStrings.length - 1];
-			 System.arraycopy(arrayOfStrings, 0, temp, 0, arrayOfStrings.length - 1);
+		} else if (arrayOfStrings.length == 2) {
+			joinString = arrayOfStrings[0] + " and " + arrayOfStrings[1];
+		} else {
+			String[] temp = new String[arrayOfStrings.length - 1];
+			System.arraycopy(arrayOfStrings, 0, temp, 0, arrayOfStrings.length - 1);
 
-			 joinString = String.join(", ", temp);
-			 joinString = joinString + " and " + arrayOfStrings[arrayOfStrings.length -1];
-		 }
-		 return joinString;
-	 }
+			joinString = String.join(", ", temp);
+			joinString = joinString + " and " + arrayOfStrings[arrayOfStrings.length - 1];
+		}
+		return joinString;
+	}
 }
